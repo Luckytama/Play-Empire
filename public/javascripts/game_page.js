a1189b96388429f9747901230cd980c4965ee4c4
Lukas Frey <lukas.lauchringen@googlemail.com>
2018-12-14 12:24:16 +0100

    Add Markup for the setup phase

---
+++ public/javascripts/game_page.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+    var players = "";
+
+    $("#btn_addPlayer").click(function() {
+        var playername = $("#input_playername").val();
+        if (playername === "") {
+            alert("form ist empty");
+            // add css to form_setup for empty input (class = "form_setup empty")
+            document.forms["form_setup"].classList.add("empty");
+        } else {
+            if (players === "") {
+                players = playername;
+            } else {
+                players += ", " + playername;
+            }
+            document.getElementById("players").innerText = players;
+        }
+    });
+
+    $("#btn_startGame").click(function() {
+        if (players === "") {
+            // add css class to form_setup for empty input or for Playingfield selection
+        } else {
+            const http = new XMLHttpRequest();
+            const url = "http://localhost:9000/empire/yay";
+            http.setRequestHeader("players", players);
+            //http.setRequestHeader("playingfield", )
+            http.open("GET", url);
+            http.send();
+        }
+    });
+
+});
