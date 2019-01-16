4565cf5bdfeb77fd8e050c4c712a80403148055c
Lukas Frey <lukas.lauchringen@googlemail.com>
2019-01-16 11:01:44 +0100

    add csrf token to ajax requests

--- public/javascripts/game_page.js
+++ public/javascripts/game_page.js
@@ -203,4 +203,18 @@ $(document).ready(function () {
         $("#player_name" + player_form_index).remove();
         player_form_index--;
     });
+
+    var csrf_token = $('input[name="csrfToken"]').val();
+    $.ajaxPrefilter(function (options) {
+        if (options.type.toLowerCase() === "post") {
+            // initialize `data` to empty string if it does not exist
+            options.data = options.data || "";
+
+            // add leading ampersand if `data` is non-empty
+            options.data += options.data ? "&" : "";
+
+            // add _token entry
+            options.data += "csrfToken=" + encodeURIComponent(csrf_token);
+        }
+    });
 });
