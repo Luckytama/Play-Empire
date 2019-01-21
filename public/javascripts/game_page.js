bc7009510fc59148c299c031ccfe9cc8991c0e8d
Lukas Frey <lukas.lauchringen@googlemail.com>
2019-01-21 11:07:34 +0100

    change server address

--- public/javascripts/game_page.js
+++ public/javascripts/game_page.js
@@ -18,7 +18,7 @@ function showNotification(isError, message) {
 
 $(document).ready(function () {
 
-    let ws = new WebSocket("ws://localhost:9000/ws");
+    let ws = new WebSocket("http://htwg-wt-empire.herokuapp.com/ws");
     //On reload functions
     window.onload = connectWebSocket(ws);
 
